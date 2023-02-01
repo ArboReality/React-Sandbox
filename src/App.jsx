3379b4795600a98f49bb34b5abe932c6adc2dd87
Arbo Reality <123933097+ArboReality@users.noreply.github.com>
2023-02-01 06:49:54 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MyComponent from './components/mycomponent'
+
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+      <MyComponent />
+    </main>
+  )
+}
