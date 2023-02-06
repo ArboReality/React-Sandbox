ce67bdc109005228d9b1355427a27a2cea8bba99
Arbo Reality <123933097+ArboReality@users.noreply.github.com>
2023-02-06 06:52:24 +0000

    added lesson files on conditional rendering, added inline styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,40 @@ import MyComponent from './components/mycomponent'
 import ControlledInput from './components/ctrlinput'
 import MyForm from './components/ctrlform'
 import Counter from './components/counter'
+import Controller from './components/lifecycles'
+import OnlyEvens from './components/lifecycles'
+import MagicEightBall from './components/eightball'
+import MyCondition from './components/conditionalrender'
 
 import FunctionalInput from './components/ctrlinput'
 import FunctionalForm from './components/ctrlform'
 import MyApp from './components/callbackprops'
 import FunctionalCounter from './components/counter';
 
-
+const styles = {
+  backgroundColor: "slategrey",
+  fontSize: ".75em",
+  textAlign: "center",
+  border: "2px solid lightblue"
+}
 
 export default function App() {
   return (
-    <main>
+    <main style={styles}>
       <MyApp />
       <ControlledInput />
       <FunctionalInput />
       <MyComponent />
       <MyForm />
       <FunctionalForm />
+      
       <Counter />
       <FunctionalCounter />
+      <Controller >
+        <OnlyEvens />
+      </Controller>
+      <MagicEightBall />
+      <MyCondition />
     </main>
   )
 }
